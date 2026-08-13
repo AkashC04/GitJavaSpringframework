6f9da99976e9d19bf514955d49981279f612ffd9
akashc <akashc@localhost>
2026-08-13 16:02:27 +0530

    change

--- src/main/java/com/shaninfotech/SpringFramework/SpringFramework/App.java
+++ src/main/java/com/shaninfotech/SpringFramework/SpringFramework/App.java
@@ -10,28 +10,28 @@ public class App {
 
     public static void main(String[] args) {
 
-     //   System.out.println("hello word");
-       //Engine engine = new Engine();
-       //Car car= new Car(engine);
-    //  car.drive();
+      System.out.println("hello word");
+       Engine engine = new Engine();
+       Car car= new Car(engine);
+     car.drive();
         
-//   	 ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-//     Car car = context.getBean(Car.class);
-//     car.drive();
-//     
-//     ApplicationContext context1 = new AnnotationConfigApplicationContext(AppConfig.class);
-//     Book book = context1.getBean(Book.class);
-//     book.bookDetails();
-
-//     ApplicationContext context2 = new AnnotationConfigApplicationContext(AppConfig.class);
-//     Vehical vehical = context2.getBean(Vehical.class);
-//     vehical.setVeh_no(2002);
-//     vehical.setVehName("supraaaaa");
-//     System.out.println(vehical.getVeh_no()+":"+ vehical.getVehName());
+ 	 ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+   Car car1 = context.getBean(Car.class);
+     car1.drive();
      
-    	ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+     ApplicationContext context1 = new AnnotationConfigApplicationContext(AppConfig.class);
+     Book book = context1.getBean(Book.class);
+     book.bookDetails();
 
-        NotificationService service = context.getBean(NotificationService.class);
+     ApplicationContext context2 = new AnnotationConfigApplicationContext(AppConfig.class);
+     Vehical vehical = context2.getBean(Vehical.class);
+     vehical.setVeh_no(2002);
+     vehical.setVehName("supraaaaa");
+     System.out.println(vehical.getVeh_no()+":"+ vehical.getVehName());
+     
+    	ApplicationContext context11 = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        NotificationService service = context11.getBean(NotificationService.class);
 
         service.notifyMessage();
     }
